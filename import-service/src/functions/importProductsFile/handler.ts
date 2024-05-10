9a244158436f08fe804e7dedb2daf607823414ce
Vadzim Yakushau <vadzim_yakushau@epam.com>
2024-05-10 16:02:28 +0200

    feat: task 6

--- import-service/src/functions/importProductsFile/handler.ts
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -1,23 +1,64 @@
-import { formatJSONResponse } from '@libs/api-gateway';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { middyfy } from '@libs/lambda';
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import type { S3Event } from 'aws-lambda';
+import parse from 'csv-parser';
+import { S3 } from '@aws-sdk/client-s3';
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
 
-const importProductsFile = async (event) => {
-    const client = new S3Client({ region: 'eu-central-1'});
+const REGION = 'eu-central-1';
+const s3 = new S3({
+    region: REGION,
+});
+const sqsClient = new SQSClient({ region: REGION });
 
-    const { name } = event.queryStringParameters;
-    const command = new PutObjectCommand({
-        Bucket: 'file-parcer-shop-stuff',
-        Key: `uploaded/${name}`,
-    });
+const importFileParser = async (
+    event: S3Event
+) => {
+    const  { s3: { object, bucket } } = event.Records[0];
+    const records = [];
+    try {
+        const stream = (await s3.getObject({
+            Bucket: bucket.name,
+            Key: object.key,
+        })).Body;
 
-    const signedUrl = await getSignedUrl(client, command, { expiresIn: 100000});
+        const parser = stream.pipe(parse());
+        for await (const record of parser) {
+            records.push(record);
+        }
 
-    return formatJSONResponse({
-        body: signedUrl,
-        event,
-    });
+        console.log(records);
+
+        const parsedKey = `parsed/${object.key.split('/')[1]}`;
+
+        console.log(parsedKey);
+        console.log("start sending messages to the queue");
+        for (const record of records) {
+            console.log('sending record:');
+
+            const result = await sqsClient.send(
+                new SendMessageCommand({
+                    QueueUrl:
+                        `https://sqs.eu-central-1.amazonaws.com/434889505863/catalogItemsQueue`,
+                    MessageBody: JSON.stringify(record),
+                })
+            );
+            console.log('result', result);
+        }
+        console.log("complete sending messages to the queue");
+
+        await s3.copyObject({
+            Bucket: bucket.name,
+            CopySource: `${bucket.name}/${object.key}`,
+            Key: parsedKey
+        }).then(() => {
+            s3.deleteObject({
+                Bucket: bucket.name,
+                Key: object.key,
+            })
+        })
+    } catch (error) {
+        console.error('Error while downloading object from S3', error.message)
+        throw error
+    }
 };
 
-export const main = middyfy(importProductsFile);
+export const main = importFileParser;
